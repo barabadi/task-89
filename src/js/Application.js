e695a12c6b669a1f09a7f4039b50b2ff242d726d
barabadi <101886037+barabadi@users.noreply.github.com>
2023-01-25 21:27:30 +0200

    Add files via upload

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,5 @@
 import EventEmitter from "eventemitter3";
+import image from "../images/planet.svg";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -7,60 +8,76 @@ export default class Application extends EventEmitter {
     };
   }
 
+  _loading = document.querySelector(".progress");
+  _startLoading() {
+    this._loading.style.visibility = "visible";
+  }
+  _stopLoading() {
+    this._loading.style.display = "none";
+    console.log("STOP")
+  }
+
   constructor() {
     super();
-    this._loading = document.getElementById("loading");
-
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
-    });
-
+    this._load();
     this.emit(Application.events.READY);
   }
 
   async _load() {
     this._startLoading();
-    let nextPage = "/planets";
+    let pageNumber = 1;
+
+    // Fetch planets
     let planets = [];
-    while (nextPage) {
-      let response = await fetch(nextPage);
-      let data = await response.json();
+    let response;
+    let data;
+    do {
+      if (pageNumber == 1) {
+        response = await fetch("https://swapi.boom.dev/api/planets");
+      } else {
+        response = await fetch("https://swapi.boom.dev/api/planets?page=" + pageNumber);
+      }
+      data = await response.json();
       planets = planets.concat(data.results);
-      nextPage = data.next;
-    }
-    this._stopLoading();
-    return planets;
-  }
+      console.log(planets)
+      pageNumber++;
+    } while (data.next);
 
-  _create(planets) {
+    // Render planets
     planets.forEach((planet) => {
-      let planetDiv = document.createElement("div");
-      planetDiv.classList.add("planet-box");
-      planetDiv.innerHTML = `
-        <h2>${planet.name}</h2>
-        <p>Population: ${planet.population}</p>
-      `;
-      planetContainer.appendChild(planetDiv);
+      this._create(planet);
     });
+
+    this._stopLoading();
   }
 
-  _startLoading() {
-    this._loading.style.display = "block";
+  _create(planet) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.innerHTML = this._render(planet);
+
+    document.body.querySelector(".main").appendChild(box);
   }
 
-  _stopLoading() {
-    this._loading.style.display = "none";
+  _render({ name, terrain, population, url }) {
+    return `
+<article class="media">
+  <div class="media-left">
+    <figure class="image is-64x64">
+      <img src="${image}" alt="planet">
+    </figure>
+  </div>
+  <div class="media-content">
+    <div class="content">
+    <h4>${name}</h4>
+      <p>
+        <span class="tag">${terrain}</span> <span class="tag">${population}</span>
+        <br>
+      </p>
+      <p class="tag">url:<a href="${url}" target="_blank">${url}</a></p>
+    </div>
+  </div>
+</article>
+    `;
   }
-}
-
-const app = new Application();
-
-app
-  ._load()
-  .then((planets) => {
-    app._create(planets);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+ }
