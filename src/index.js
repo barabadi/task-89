b6a9e1ccb99ede7b9aedcea4aca3366d63e5670f
barabadi <101886037+barabadi@users.noreply.github.com>
2023-01-25 22:59:41 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./scss/app.scss";
-import Application from "./js/Application.js";
+import Application from "./js/Application";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
