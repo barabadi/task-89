7001e1259d187bf018495ddf3ee88bd172bfaeab
barabadi <101886037+barabadi@users.noreply.github.com>
2023-01-31 19:49:16 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Application from "./js/Application";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const app = new Application();
-  const Application = window._JS_app;
   // Used to access the app instance by the automated tests
   window.__JS_APP = app;
 });
